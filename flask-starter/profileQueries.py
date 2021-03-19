6fcaa3e90ecb5b613564b072d0b4f6322284e80a
Shreya Kochar <sk1@cs.wellesley.edu>
2021-03-19 02:07:49 -0400

    fixed

--- flask-starter/profileQueries.py
+++ flask-starter/profileQueries.py
@@ -37,11 +37,11 @@ def insert_professionalInterests(conn, wemail, industry, dreamJob):
     if len(checkInt) == 0: 
         curs.execute(f'INSERT INTO professionalInterests (wemail, \
             industry, dreamJob) VALUES ({wemail}, {industry}, \
-                {dreamJob})')
+            {dreamJob})')
         conn.commit()
 
 def update_professionalInterests(conn, wemail, industry, dreamJob):
-    # update professional interests list 
+    # update professional interests list
     curs = dbi.dict_cursor(conn)
     wemail = f'''"{wemail}"''' if wemail else "NULL"
     curs.execute(f'UPDATE professionalInterests SET industry = {industry} \
@@ -74,69 +74,6 @@ def update_professionalInterests(conn, wemail, industry, dreamJob):
         WHERE wemail = {wemail}')
     conn.commit()
 
-
-def update_addedby(conn, tt, addedby):
-    curs = dbi.dict_cursor(conn)
-    addedby = f'''"{addedby}"''' if addedby else "NULL"
-    curs.execute(f'UPDATE movie SET addedby = {addedby} where tt = {tt}')
-    conn.commit()
-
-def update_title(conn, tt, title):
-    curs = dbi.dict_cursor(conn)
-    title = f'''"{title}"''' if title else "NULL"
-    curs.execute(f'UPDATE movie SET title = {title} where tt = {tt}')
-    conn.commit()
-
-def update_release(conn, tt, release):
-    curs = dbi.dict_cursor(conn)
-    release = f'''"{release}"''' if release else "NULL"
-    curs.execute(f'UPDATE movie SET `release` = {release} where tt = {tt}')
-    conn.commit()
-
-def update_tt(conn, oldTT, newTT): # last so it doesn't mess other queries
-    newTT = int(newTT)
-    if not (oldTT == newTT):
-        curs = dbi.dict_cursor(conn)
-        curs.execute(f'SELECT tt FROM movie WHERE tt = {newTT}')
-        newInfo = curs.fetchall()
-        if not newInfo:
-            curs.execute(f'UPDATE movie SET tt = {newTT} where tt = {oldTT}')
-            conn.commit()
-            return newTT
-    elif oldTT == newTT: # do nothing if they're the same value
-        return "Do nothing" 
-    return None # newTT != oldTT and newTT already exists in db
-
-def movie(conn, tt):
-    '''Finds all attributes of movie with the tt (id) specified'''
-    curs = dbi.dict_cursor(conn)
-    curs.execute(f'select * from movie where tt = {tt}')
-    return curs.fetchall()
-
-def dirName(conn, tt):
-    curs = dbi.dict_cursor(conn)
-    curs.execute(f'SELECT director, name from person inner join movie on \
-        (director = nm) where tt = {tt}')
-    directorName = curs.fetchall()
-    if directorName:
-        return directorName
-    else:
-        return None # no director name displayed
-
-def deleteEntry(conn, tt):
-    curs = dbi.dict_cursor(conn)
-    curs.execute(f'SELECT * from movie where tt = {tt}')
-    entry = curs.fetchall()
-    if entry:
-        curs.execute(f'DELETE from movie where tt = {tt}')
-    conn.commit()
-
-def get_incomplete_movies(conn):
-    curs = dbi.dict_cursor(conn)
-    sql = 'SELECT * from movie where \
-        director is NULL or `release` is NULL'
-    curs.execute(sql)
-    return curs.fetchall()
 
 
 if __name__ == '__main__':
