d70ced789974b8a956503b17407bf4efcd983475
skochar1 <60591774+skochar1@users.noreply.github.com>
2021-03-20 20:44:43 -0700

    Update makeMatchesQueries.py

--- makeMatchesQueries.py
+++ makeMatchesQueries.py
@@ -10,7 +10,8 @@ import scoreQueries
 def insertScores(conn, wemail):
     '''Inserts initial match scores into every person in the databases's 
     matches table. This should be done as soon as the person's account is 
-    created, without the user having to recallibrate their matches manually.'''
+    created, without the user having to recallibrate their matches manually,
+    works given the current user's wemail.'''
     curs = dbi.dict_cursor(conn)
     curs.execute('SELECT wemail from userAccount') # gives every person in database
     allUsers = curs.fetchall() # assuming allUsers is a list of dicts
@@ -22,16 +23,17 @@ def insertScores(conn, wemail):
             score = scoreQueries.matchScore(conn, emailID, wemail)
 
             # insert current user wemail to emailID row
-            curs.execute(f'INSERT INTO matches_scored (wemail, wemail2, score) \
-            VALUES ({wemail}, {emailID}, {score})')
+            curs.execute(f'INSERT INTO matches_scored (wemail, wemail2, score, \
+            isMatched) VALUES ({wemail}, {emailID}, {score}, "no")')
 
             # insert emailID to current user wemail row
-            curs.execute(f'INSERT INTO matches_scored (wemail, wemail2, score) \
-            VALUES ({emailID}, {wemail}, {score})')
+            curs.execute(f'INSERT INTO matches_scored (wemail, wemail2, score, \
+            isMatched) VALUES ({emailID}, {wemail}, {score}, "no")')
             
 
 def updateScores(conn, wemail):
-    '''Only used if the user updates their profile. '''
+    '''Only used if the current user updates their profile,
+    works given the current user's wemail. '''
     curs = dbi.dict_cursor(conn)
     curs.execute('SELECT wemail from userAccount') # gives every person in database
     allUsers = curs.fetchall() # assuming allUsers is a list of dicts
@@ -50,13 +52,38 @@ def updateScores(conn, wemail):
             curs.execute(f'UPDATE matches_scored SET score = {score} WHERE wemail \
                  = {emailID} and wemail2 = {wemail}')
 
-def generateMatches(conn, wemail):
+def generatePotentialMatches(conn, wemail): 
     '''Generates the matches for the current user given their wemail.
     Sorted by highest matching score to lowest matching score.'''
     curs = dbi.dict_cursor(conn)
     curs.execute(f'SELECT * FROM matches_scored WHERE wemail = {wemail} \
         ORDER BY score DESC')
     return curs.fetchall() # returns table of matches
+
+def isMatched(conn, wemail, wemail2):
+    '''Takes current user and their matched person's info and updates
+    both of their matched status accordingly; wemail is the current user,
+    wemail2 is the matched person'''
+    curs = dbi.dict_cursor(conn)
+        # update matching status for user's row
+    curs.execute(f'UPDATE matches_scored SET isMatched = "yes" WHERE wemail \
+        = {wemail} and wemail2 = {wemail2}')
+
+        # update matching status for matched person's row
+    curs.execute(f'UPDATE matches_scored SET isMatched = "yes" WHERE wemail \
+        = {wemail2} and wemail2 = {wemail}')
+    return curs.fetchall()
+
+def getMatches(conn, wemail):
+    '''Returns the information of the people the user has matched with,
+    given the user's wemail'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute(f'SELECT b.wemail, b.fname, b.lname, b.year
+    FROM userAccount as a INNER JOIN matches_scored m ON (m.wemail \
+    = a.wemail) INNER JOIN userAccount as b ON (m.wemail2 = b.wemail)
+    WHERE m.isMatched = "yes" and m.wemail = {wemail}') 
+
+    return curs.fetchall()
 
 
 if __name__ == '__main__':
