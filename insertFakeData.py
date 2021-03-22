f8d62fd6cfdc81d48a9d13c1888c94167c291ceb
Match <wellesleymatch@cs.wellesley.edu>
2021-03-22 11:15:30 -0400

    small changes to csv info

--- insertFakeData.py
+++ insertFakeData.py
@@ -68,25 +68,6 @@ def insertDataToTables(conn,filename):
             "{row[fieldsDict["language"]]}")')
             conn.commit()
 
-            #insert data into bio
-            #curs.execute(f'insert into bio(wemail,bioID,bio) \
-            #values("{row[fieldsDict["wemail"]]}","{row[fieldsDict["bioID"]]}",\
-            #"{row[fieldsDict["bio"]]}")')
-            #conn.commit()
-
-
-# def getIceBreaker():
-#     '''Randomly returns an ice breaker'''
-#     iceBreakers = ["Two Truths and One Lie", 
-#                     "How are you feeling today?", 
-#                     "Get the weirdest thing in your room, then bring it back to show",
-#                     "Highlight of the month?",
-#                     "Failure of the month?",
-#                     "Play three rounds of Never Have I Ever",
-#                     "Virtual wine tasting!",
-#                     "What’s the last picture that you took?"]
-#     pick = random.randint(0, len(iceBreaker)-1)
-#     return iceBreaker[pick]
 
 if __name__ == '__main__':
     filename = "users.csv"
